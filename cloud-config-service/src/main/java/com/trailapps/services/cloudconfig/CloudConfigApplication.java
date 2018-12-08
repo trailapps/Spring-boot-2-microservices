dd429d97594d9629abc952f0209e849bcf60585f
TrailApps <criticalhead46@outlook.com>
2018-12-08 15:00:10 +0530

    added eureka discovery service

--- cloud-config-service/src/main/java/com/trailapps/services/cloudconfig/CloudConfigApplication.java
+++ cloud-config-service/src/main/java/com/trailapps/services/cloudconfig/CloudConfigApplication.java
@@ -2,12 +2,13 @@ package com.trailapps.services.cloudconfig;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
 public class CloudConfigApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CloudConfigApplication.class, args);
 	}
 }
-vie
